0fbe7f56c1f2768386df61c5080f4014d5065b6b
TjyGitHub1805099 <912709177@qq.com>
2021-07-17 16:52:35 +0800

    MCU Relase20210717 Modified Larger Value Jump and Help Data

---
+++ WeightMode_V3/SoftWare/STM32MCU/Release20210717/firm_wm_00_app/inc/app_main_task.h
@@ -0,0 +1,13 @@
+#ifndef __APP_MAIN_TASK_H__
+#define __APP_MAIN_TASK_H__
+
+#include "typedefine.h"
+#include "app_led_ctrl.h"
+
+#define COLOR_ALT_20210328_DEFINE	(FALSE)
+#define COLOR_ALT_20210414_DEFINE	(FALSE)
+#define COLOR_ALT_20210427_DEFINE	(FALSE)
+#define COLOR_ALT_20210606_DEFINE	(FALSE)
+
+
+#endif
