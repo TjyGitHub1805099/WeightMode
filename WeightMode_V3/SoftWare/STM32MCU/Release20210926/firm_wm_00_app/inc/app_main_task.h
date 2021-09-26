774a68bb70e998b116493885d192dc178f995fe6
TjyGitHub1805099 <912709177@qq.com>
2021-09-26 20:38:24 +0800

    20210926 [MCU] , when err was equal to err_range allow it .

---
+++ WeightMode_V3/SoftWare/STM32MCU/Release20210926/firm_wm_00_app/inc/app_main_task.h
@@ -0,0 +1,15 @@
+#ifndef __APP_MAIN_TASK_H__
+#define __APP_MAIN_TASK_H__
+
+#include "typedefine.h"
+#include "app_led_ctrl.h"
+
+#define COLOR_ALT_20210328_DEFINE	(FALSE)
+#define COLOR_ALT_20210414_DEFINE	(FALSE)
+#define COLOR_ALT_20210427_DEFINE	(FALSE)
+#define COLOR_ALT_20210606_DEFINE	(FALSE)
+
+#define SYS_POWER_REDAY_TIME		(3500)//3500ms
+#define SYS_HX711_ONLINE_CHECK_TIME	(SYS_POWER_REDAY_TIME-1200)//2300ms
+
+#endif
