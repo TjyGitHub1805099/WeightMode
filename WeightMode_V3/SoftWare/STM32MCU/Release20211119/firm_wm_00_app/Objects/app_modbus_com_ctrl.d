363dc0848c636374701b315b7f99194bd9d91502
TjyGitHub1805099 <912709177@qq.com>
2021-11-19 21:33:56 +0800

    20211119 [MCU] [SCREEN] add syspara entry use password , add screen light ctrl , add syspara min and max

---
+++ WeightMode_V3/SoftWare/STM32MCU/Release20211119/firm_wm_00_app/Objects/app_modbus_com_ctrl.d
@@ -0,0 +1,8 @@
+.\objects\app_modbus_com_ctrl.o: src\app_modbus_com_ctrl.c
+.\objects\app_modbus_com_ctrl.o: ..\STM32F303DriverLib\inc\drv_flash.h
+.\objects\app_modbus_com_ctrl.o: ..\STM32F303DriverLib\reg\reg_stm32f303_flash.h
+.\objects\app_modbus_com_ctrl.o: ..\STM32F303DriverLib\reg\typedefine.h
+.\objects\app_modbus_com_ctrl.o: ..\STM32F303DriverLib\reg\reg_stm32f303_address.h
+.\objects\app_modbus_com_ctrl.o: .\inc\app_main_task.h
+.\objects\app_modbus_com_ctrl.o: .\inc\app_led_ctrl.h
+.\objects\app_modbus_com_ctrl.o: .\inc\app_main_task.h
